ebdf72ace0a4c955da0702b1abb09b83d1ebd91b
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2022-03-12 23:13:26 +0300

    Logger added to MainController

--- src/main/java/tech/sergeyev/compmechlabauth/api/MainController.java
+++ src/main/java/tech/sergeyev/compmechlabauth/api/MainController.java
@@ -1,5 +1,8 @@
 package tech.sergeyev.compmechlabauth.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class MainController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
     @GetMapping
     public String index() {
-
+        LOGGER.info("GET request received");
         return "index";
     }
 }
