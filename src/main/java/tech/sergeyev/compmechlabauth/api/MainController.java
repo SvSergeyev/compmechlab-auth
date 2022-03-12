50e1c0840fbfdfc6268f3f50ce385eab35ec8eeb
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2022-03-13 01:06:12 +0300

    ldap port added

--- src/main/java/tech/sergeyev/compmechlabauth/api/MainController.java
+++ src/main/java/tech/sergeyev/compmechlabauth/api/MainController.java
@@ -8,14 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.http.HttpHeaders;
+
 @RestController
 @RequestMapping("/")
 public class MainController {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
     @GetMapping
-    public String index() {
-        LOGGER.info("GET request received");
+    public String index(HttpHeaders headers) {
+        LOGGER.info("GET request received{}", headers.toString());
         return "index";
     }
 }
