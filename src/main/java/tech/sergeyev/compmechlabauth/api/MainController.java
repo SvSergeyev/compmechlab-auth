74a7b8b850f4c63f52868680b17362bbe3fdd0a3
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2022-03-13 14:49:55 +0300

    unused imports removed

--- src/main/java/tech/sergeyev/compmechlabauth/api/MainController.java
+++ src/main/java/tech/sergeyev/compmechlabauth/api/MainController.java
@@ -2,14 +2,9 @@ package tech.sergeyev.compmechlabauth.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.http.HttpHeaders;
-import java.net.http.HttpRequest;
 
 @RestController
 @RequestMapping("/")
